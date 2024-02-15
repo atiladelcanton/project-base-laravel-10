027ac6b8bcf0d6aff43c6b827b022922a293f6c3
Atila Rampazo <atilarampazo@gmail.com>
2024-02-15 09:14:38 -0300

    Add

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,10 @@
+<x-app-layout>
+    <x-slot name="header">
+        <x-header>
+            {{ __('Dashboard') }}
+        </x-header>
+    </x-slot>
+    <x-container>
+
+    </x-container>
+</x-app-layout>
